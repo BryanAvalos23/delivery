d177d379fe7a725d04b227a238ee975f835a62db
Bryan Avalos <trollerepik@gmail.com>
2024-03-17 16:57:07 -0600

    feat: add themes light and dark

---
+++ lib/themes/theme_provider.dart
@@ -0,0 +1,22 @@
+import 'package:delivery/themes/dark_mode.dart';
+import 'package:delivery/themes/light_mode.dart';
+import 'package:flutter/material.dart';
+
+class ThemeProvider with ChangeNotifier {
+  ThemeData _themeData = lightMode;
+  ThemeData get themeData => _themeData;
+  bool get isDarkMode => _themeData == darkMode;
+
+  set themeData(ThemeData themeData) {
+    _themeData = themeData;
+    notifyListeners();
+  }
+
+  void toggleVoid() {
+    if (_themeData == lightMode) {
+      themeData = darkMode;
+    } else {
+      themeData = lightMode;
+    }
+  }
+}
