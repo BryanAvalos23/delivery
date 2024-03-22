bc6980140fac1c53de9e373992b8fd5ae6618edf
Bryan Avalos <trollerepik@gmail.com>
2024-03-22 14:41:16 -0600

    feat: add alert dialog for payment

--- lib/pages/payment_page.dart
+++ lib/pages/payment_page.dart
@@ -1,3 +1,4 @@
+import 'package:delivery/components/button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_credit_card/flutter_credit_card.dart';
 
@@ -9,11 +10,61 @@ class PaymentPage extends StatefulWidget {
 }
 
 class _PaymentPageState extends State<PaymentPage> {
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
   String cardNumber = '';
   String expiryDate = '';
   String cardHolderName = '';
   String cvvCode = '';
   bool isCvvFocused = false;
+
+  // user wants to pay
+  void userTappedPay() {
+    if (formKey.currentState!.validate()) {
+      // only show dialog if form is valid
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text("Confirm payment"),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: [
+                Text(
+                  "Card Number: $cardNumber",
+                ),
+                Text(
+                  "Expiry Date: $expiryDate",
+                ),
+                Text(
+                  "Card holder name: $cardHolderName",
+                ),
+                Text(
+                  "CVV: $cvvCode",
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            // cancel button
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text("Cancel"),
+            ),
+
+            // acepte button
+            TextButton(
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DeliveryProgressPage(),
+                ),
+              ),
+              child: const Text("Acept"),
+            ),
+          ],
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,6 +88,32 @@ class _PaymentPageState extends State<PaymentPage> {
           ),
 
           // credit card form
+          CreditCardForm(
+            cardNumber: cardNumber,
+            expiryDate: expiryDate,
+            cardHolderName: cardHolderName,
+            cvvCode: cvvCode,
+            onCreditCardModelChange: (data) {
+              setState(() {
+                cardNumber = data.cardNumber;
+                expiryDate = data.expiryDate;
+                cardHolderName = data.cardHolderName;
+                cvvCode = data.cvvCode;
+              });
+            },
+            formKey: formKey,
+          ),
+
+          const Spacer(),
+
+          MyButton(
+            text: "Pay now",
+            onTap: userTappedPay,
+          ),
+
+          const SizedBox(
+            height: 25,
+          ),
         ],
       ),
     );
